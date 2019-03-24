7a72e59d7aaf43a7700b44729c3a4d6f75395dc1
wh1t3cAt1k <pavel@kabir.ru>
2019-03-24 21:04:57 +0300

    Remove deprecated rules

--- tslint-config.js
+++ tslint-config.js
@@ -1,189 +1,189 @@
 module.exports = {
-    "defaultSeverity": "error",
-    "extends": [
-        "tslint:recommended",
-        "tslint-eslint-rules",
-        "tslint-react",
-        "tslint-sonarts",
-        "tslint-microsoft-contrib",
-        "tslint-consistent-codestyle"
+    'defaultSeverity': 'error',
+    'extends': [
+        'tslint:recommended',
+        'tslint-eslint-rules',
+        'tslint-react',
+        'tslint-sonarts',
+        'tslint-microsoft-contrib',
+        'tslint-consistent-codestyle'
     ],
-    "rules": {
-        "arguments-order": true,
-        "array-bracket-spacing": [
+    'rules': {
+        'arguments-order': true,
+        'array-bracket-spacing': [
             true,
-            "never"
+            'never'
         ],
-        "arrow-parens": [
+        'arrow-parens': [
             true,
-            "ban-single-arg-parens"
+            'ban-single-arg-parens'
         ],
-        "ban-comma-operator": true,
-        "binary-expression-operand-order": false,
-        "block-spacing": [
+        'ban-comma-operator': true,
+        'binary-expression-operand-order': false,
+        'block-spacing': [
             true,
-            "always"
+            'always'
         ],
-        "brace-style": [
+        'brace-style': [
             true,
-            "1tbs",
+            '1tbs',
             {
-                "allowSingleLine": true
+                'allowSingleLine': true
             }
         ],
-        "bool-param-default": false,
-        "completed-docs": false,
-        "early-exit": [
+        'bool-param-default': false,
+        'completed-docs': false,
+        'early-exit': [
             true,
             {
-                "max-length": 4
+                'max-length': 4
             }
         ],
-        "encoding": true,
-        "file-name-casing": [true, "pascal-case"],
-        "function-name": [
+        'encoding': true,
+        'file-name-casing': [true, 'pascal-case'],
+        'function-name': [
             true,
             {
-                "function-regex": "^[a-z][\\w\\d]+$",
-                "method-regex": "^[a-z][\\w\\d]+$",
-                "private-method-regex": "^[a-z][\\w\\d]+$",
-                "protected-method-regex": "^[a-z][\\w\\d]+$",
-                "static-method-regex": "^[a-z][A-Za-z\\d]+$"
+                'function-regex': '^[a-z][\\w\\d]+$',
+                'method-regex': '^[a-z][\\w\\d]+$',
+                'private-method-regex': '^[a-z][\\w\\d]+$',
+                'protected-method-regex': '^[a-z][\\w\\d]+$',
+                'static-method-regex': '^[a-z][A-Za-z\\d]+$'
             }
         ],
-        "handle-callback-err": [
+        'handle-callback-err': [
             true,
-            "^(err|error)$"
+            '^(err|error)$'
         ],
-        "import-name": false,
-        "increment-decrement": [true, "allow-post"],
-        "indent": [
+        'import-name': false,
+        'increment-decrement': [true, 'allow-post'],
+        'indent': [
             true,
-            "spaces",
+            'spaces',
             4
         ],
-        "interface-name": [
+        'interface-name': [
             true,
-            "always-prefix"
+            'always-prefix'
         ],
-        "jasmine-no-lambda-expression-callbacks": false,
-        "jsx-boolean-value": [
+        'jasmine-no-lambda-expression-callbacks': false,
+        'jsx-boolean-value': [
             true,
-            "never"
+            'never'
         ],
-        "jsx-curly-spacing": [true, "never"],
-        "jsx-equals-spacing": [true, "never"],
-        "jsx-no-bind": true,
-        "jsx-self-close": true,
-        "jsx-space-before-trailing-slash": true,
-        "jsx-wrap-multiline": true,
-        "linebreak-style": false,
-        "max-classes-per-file": [
+        'jsx-curly-spacing': [true, 'never'],
+        'jsx-equals-spacing': [true, 'never'],
+        'jsx-no-bind': true,
+        'jsx-self-close': true,
+        'jsx-space-before-trailing-slash': true,
+        'jsx-wrap-multiline': true,
+        'linebreak-style': false,
+        'max-classes-per-file': [
             true,
             1,
-            "exclude-class-expressions"
+            'exclude-class-expressions'
         ],
-        "max-line-length": [
+        'max-line-length': [
             true,
             100
         ],
-        "member-access": [
+        'member-access': [
             true,
-            "check-accessor",
-            "check-constructor"
+            'check-accessor',
+            'check-constructor'
         ],
-        "missing-jsdoc": false,
-        "mocha-no-side-effect-code": [
+        'missing-jsdoc': false,
+        'mocha-no-side-effect-code': [
             true,
             {
-                "ignore": "nameof|createMockStore|it"
+                'ignore': 'nameof|createMockStore|it'
             }
         ],
-        "naming-convention": [
+        'naming-convention': [
             true,
             {
-                "type": "default",
-                "format": "camelCase",
-                "leadingUnderscore": "forbid",
-                "trailingUnderscore": "forbid"
+                'type': 'default',
+                'format': 'camelCase',
+                'leadingUnderscore': 'forbid',
+                'trailingUnderscore': 'forbid'
             },
             {
-                "type": "variable",
-                "format": "camelCase"
+                'type': 'variable',
+                'format': 'camelCase'
             },
             {
-                "type": "variable",
-                "modifiers": [
-                    "global",
-                    "const"
+                'type': 'variable',
+                'modifiers': [
+                    'global',
+                    'const'
                 ],
-                "format": [
-                    "camelCase",
-                    "PascalCase"
+                'format': [
+                    'camelCase',
+                    'PascalCase'
                 ]
             },
             {
-                "type": "variable",
-                "modifiers": [
-                    "export",
-                    "const"
+                'type': 'variable',
+                'modifiers': [
+                    'export',
+                    'const'
                 ],
-                "format": [
-                    "camelCase",
-                    "PascalCase"
+                'format': [
+                    'camelCase',
+                    'PascalCase'
                 ]
             },
             {
-                "type": "member",
-                "format": [
-                    "camelCase",
-                    "PascalCase"
+                'type': 'member',
+                'format': [
+                    'camelCase',
+                    'PascalCase'
                 ]
             },
             {
-                "type": "type",
-                "format": "StrictPascalCase"
+                'type': 'type',
+                'format': 'StrictPascalCase'
             },
             {
-                "type": "parameter",
-                "modifiers": "unused",
-                "leadingUnderscore": "allow"
+                'type': 'parameter',
+                'modifiers': 'unused',
+                'leadingUnderscore': 'allow'
             },
             {
-                "type": "interface",
-                "prefix": "I"
+                'type': 'interface',
+                'prefix': 'I'
             },
             {
-                "type": "genericTypeParameter",
-                "prefix": "T"
+                'type': 'genericTypeParameter',
+                'prefix': 'T'
             },
             {
-                "type": "enumMember",
-                "format": "PascalCase"
+                'type': 'enumMember',
+                'format': 'PascalCase'
             }
         ],
-        "newline-per-chained-call": true,
-        "no-backbone-get-set-outside-model": false,
-        "no-big-function": [true, 300],
-        "no-boolean-literal-compare": true,
-        "no-collapsible-if": true,
-        "no-constant-condition": true,
-        "no-control-regex": true,
-        "no-dead-store": true,
-        "no-default-import": true,
-        "no-duplicate-imports": true,
-        "no-empty-array": true,
-        "no-ex-assign": true,
-        "no-extra-boolean-cast": true,
-        "no-extra-semi": true,
-        "no-implicit-dependencies": [true, "dev"],
-        "no-inferrable-types": true,
-        "no-inner-declarations": true,
-        "no-invalid-regexp": true,
-        "no-invalid-template-strings": true,
-        "no-invalid-this": true,
-        "no-invariant-return": true,
-        "no-magic-numbers": [
+        'newline-per-chained-call': true,
+        'no-backbone-get-set-outside-model': false,
+        'no-big-function': [true, 300],
+        'no-boolean-literal-compare': true,
+        'no-collapsible-if': true,
+        'no-constant-condition': true,
+        'no-control-regex': true,
+        'no-dead-store': true,
+        'no-default-import': true,
+        'no-duplicate-imports': true,
+        'no-empty-array': true,
+        'no-ex-assign': true,
+        'no-extra-boolean-cast': true,
+        'no-extra-semi': true,
+        'no-implicit-dependencies': [true, 'dev'],
+        'no-inferrable-types': true,
+        'no-inner-declarations': true,
+        'no-invalid-regexp': true,
+        'no-invalid-template-strings': true,
+        'no-invalid-this': true,
+        'no-invariant-return': true,
+        'no-magic-numbers': [
             true,
             -1,
             0,
@@ -203,102 +203,114 @@ module.exports = {
             1000,
             2000
         ],
-        "no-multi-spaces": true,
-        "no-namespace": true,
-        "no-non-null-assertion": false,
-        "no-null-keyword": true,
-        "no-object-literal-type-assertion": false,
-        "no-parameter-properties": true,
-        "no-regex-spaces": true,
-        "no-relative-imports": false,
-        "no-require-imports": false,
-        "no-static-this": true,
-        "no-submodule-imports": [
+        'no-multi-spaces': true,
+        'no-namespace': true,
+        'no-non-null-assertion': false,
+        'no-null-keyword': true,
+        'no-object-literal-type-assertion': false,
+        'no-parameter-properties': true,
+        'no-regex-spaces': true,
+        'no-relative-imports': false,
+        'no-require-imports': false,
+        'no-static-this': true,
+        'no-submodule-imports': [
             true,
-            "redux-saga-test-plan/matchers",
-            "redux-saga-test-plan/providers",
-            "redux-saga/effects"
+            'redux-saga-test-plan/matchers',
+            'redux-saga-test-plan/providers',
+            'redux-saga/effects'
         ],
-        "no-suspicious-comment": false,
-        "no-trailing-whitespace": true,
-        "no-unexpected-multiline": true,
-        "no-unnecessary-callback-wrapper": false,
-        "no-unnecessary-class": false,
-        "no-unnecessary-else": true,
-        "no-unnecessary-qualifier": true,
-        "no-unsafe-any": false,
-        "no-unused-expression": true,
-        "no-use-before-declare": false,
-        "no-useless-catch": true,
-        "no-var-before-return": true,
-        "no-void-expression": [
+        'no-suspicious-comment': false,
+        'no-trailing-whitespace': true,
+        'no-unexpected-multiline': true,
+        'no-unnecessary-callback-wrapper': false,
+        'no-unnecessary-class': false,
+        'no-unnecessary-else': true,
+        'no-unnecessary-qualifier': true,
+        'no-unsafe-any': false,
+        'no-unused-expression': true,
+        'no-use-before-declare': false,
+        'no-useless-catch': true,
+        'no-var-before-return': true,
+        'no-void-expression': [
             true,
-            "ignore-arrow-function-shorthand"
+            'ignore-arrow-function-shorthand'
         ],
-        "object-literal-sort-keys": false,
-        "ordered-imports": true,
-        "prefer-const-enum": false,
-        "prefer-method-signature": true,
+        'object-literal-sort-keys': false,
+        'ordered-imports': true,
+        'prefer-const-enum': false,
+        'prefer-method-signature': true,
         /*
         In some cases we want to force the user to think about a prop and either
         explicitly pass a value or explicitly pass an undefined. Optional syntax
         would allow the user to omit the value altogether without any compiler warnings.
         */
-        "prefer-optional": false,
-        "quotemark": [
+        'prefer-optional': false,
+        'quotemark': [
             true,
-            "single",
-            "avoid-escape",
-            "avoid-template"
+            'single',
+            'avoid-escape',
+            'avoid-template'
         ],
-        "space-in-parens": true,
-        "space-within-parens": [
+        'space-in-parens': true,
+        'space-within-parens': [
             true,
             0
         ],
-        "strict-boolean-expressions": true,
-        "switch-default": true,
-        "switch-final-break": false,
-        "ter-arrow-body-style": [
+        'strict-boolean-expressions': true,
+        'switch-default': true,
+        'switch-final-break': false,
+        'ter-arrow-body-style': [
             true,
-            "as-needed"
+            'as-needed'
         ],
-        "ter-arrow-spacing": true,
-        "ter-func-call-spacing": true,
-        "ter-indent": [
+        'ter-arrow-spacing': true,
+        'ter-func-call-spacing': true,
+        'ter-indent': [
             true,
             4,
             {
-                "SwitchCase": 1
+                'SwitchCase': 1
             }
         ],
-        "ter-no-mixed-spaces-and-tabs": {
-            "type": "spaces"
+        'ter-no-mixed-spaces-and-tabs': {
+            'type': 'spaces'
         },
-        "ter-no-proto": true,
-        "ter-no-self-compare": true,
-        "ter-no-sparse-arrays": true,
-        "ter-no-tabs": true,
-        "ter-padded-blocks": [
+        'ter-no-proto': true,
+        'ter-no-self-compare': true,
+        'ter-no-sparse-arrays': true,
+        'ter-no-tabs': true,
+        'ter-padded-blocks': [
             true,
-            "never"
+            'never'
         ],
-        "trailing-comma": [
+        'trailing-comma': [
             true,
             {
-                "multiline": {
-                    "arrays": "always",
-                    "functions": "never",
-                    "objects": "always",
-                    "typeLiterals": "ignore"
+                'multiline': {
+                    'arrays': 'always',
+                    'functions': 'never',
+                    'objects': 'always',
+                    'typeLiterals': 'ignore'
                 },
-                "singleline": "never"
+                'singleline': 'never'
             }
         ],
-        "triple-equals": true,
-        "typedef": false,
-        "underscore-consistent-invocation": false,
-        "unnecessary-bind": true,
-        "variable-name": false
+        'triple-equals': true,
+        'typedef': false,
+        'underscore-consistent-invocation': false,
+        'unnecessary-bind': true,
+        'variable-name': false,
+        // Deprecated
+        // -
+        'no-reserved-keywords': false,
+        // Deprecated
+        // -
+        'no-function-constructor-with-string-args': false,
+        // Deprecated
+        // -
+        'no-increment-decrement': false,
+        // Deprecated
+        // -
+        'no-unnecessary-bind': false,
     }
 }
